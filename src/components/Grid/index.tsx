2b006cd247a66a2c73adb72cbe20d865fcd7fe0d
p1t1ch <belobeev.kirill@gmail.com>
2021-01-22 15:44:20 +0300

    little offsets tweak

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -4,8 +4,8 @@ function Grid() {
   return (
     <article className="grid sm:grid-cols-2 max-w-lg bg-neutral shadow-grid rounded-lg overflow-hidden">
       <section className="col-span-full p-6 pt-7 pb-8 sm:p-10">
-        <h1 className="text-xl sm:text-2xl font-bold text-primary mb-4">Join our community</h1>
-        <div className="text-15 sm:text-lg font-bold text-primary-light mb-3">
+        <h1 className="text-xl sm:text-2xl font-bold text-primary mb-5">Join our community</h1>
+        <div className="text-15 sm:text-lg font-bold text-primary-light mb-2">
           30-day, hassle-free money back guarantee
         </div>
         <p className="text-sm sm:text-base">
@@ -15,11 +15,11 @@ function Grid() {
       </section>
       <section className="p-6 sm:p-10 bg-primary text-neutral">
         <h2 className="font-bold text-lg mb-4">Monthly Subscription</h2>
-        <div className="mb-1">
+        <div className="mb-2">
           <span className="font-bold text-3xl align-middle mr-2">$29</span>&nbsp;
           <span className="text-neutral text-opacity-50">per month</span>
         </div>
-        <div className="mb-6">Full access for less than $1 a day</div>
+        <div className="mb-7">Full access for less than $1 a day</div>
         <button className="h-12 w-full bg-primary-light hover:bg-primary-hover transition-colors shadow-button rounded-button">
           Sign Up
         </button>
